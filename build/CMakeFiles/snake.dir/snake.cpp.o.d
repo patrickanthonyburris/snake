167cdeac2e3d09f42d1135302a6511167fcc73b6
patrickanthonyburris <patrickanthonyburris@gmail.com>
2026-07-11 11:00:17 -0700

    Delete build directory

--- build/CMakeFiles/snake.dir/snake.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/snake.dir/snake.cpp.o: /home/pburris/projects/snake/snake.cpp \
- /usr/include/stdc-predef.h /home/pburris/projects/snake/snake.hpp
--- build/CMakeFiles/snake.dir/space.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/snake.dir/space.cpp.o: /home/pburris/projects/snake/space.cpp \
- /usr/include/stdc-predef.h /home/pburris/projects/snake/space.hpp
